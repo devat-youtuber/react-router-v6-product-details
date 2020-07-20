edb7f2802334124455ccfaf01c042dcbb309b675
dev at <battuosai01@gmail.com>
2020-07-20 10:26:43 +0700

    Updates

---
+++ precache-manifest.a3003fba05ec7ed3d127b944d5f76f92.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2dd9c03d8d6961d870aacdab69a004b",
+    "url": "/react-router-v6-product-details/index.html"
+  },
+  {
+    "revision": "9140c90c329189ef4ef6",
+    "url": "/react-router-v6-product-details/static/css/main.63759396.chunk.css"
+  },
+  {
+    "revision": "789a72891d527b6f4dc8",
+    "url": "/react-router-v6-product-details/static/js/2.9ac1820c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-router-v6-product-details/static/js/2.9ac1820c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9140c90c329189ef4ef6",
+    "url": "/react-router-v6-product-details/static/js/main.287f5e6f.chunk.js"
+  },
+  {
+    "revision": "720496312f920343f493",
+    "url": "/react-router-v6-product-details/static/js/runtime-main.3154b31d.js"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/react-router-v6-product-details/static/media/bars-solid.b3f49afd.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/react-router-v6-product-details/static/media/times-solid.2c5552a1.svg"
+  }
+]);
